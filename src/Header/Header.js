19a7527a36190c793f5948eb7371de1555d17a07
ivayloK <qwerty36758@gmail.com>
2024-05-06 18:06:43 +0300

    Created Header component [not finished]

---
+++ src/Header/Header.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Header.css';
+
+export default function Header() {
+	return (
+		<header className='header'>
+			<nav className='header-nav'>
+				<ul>
+					<li>A</li>
+					<li>A</li>
+					<li>A</li>
+					<li>A</li>
+				</ul>
+			</nav>
+		</header>
+	);
+}
